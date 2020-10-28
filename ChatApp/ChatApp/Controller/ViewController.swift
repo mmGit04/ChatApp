f577bf2844f04fdf7f0732f799252c394b57178f
Mina <m.milosavljevic@levi9.com>
2020-10-28 18:30:27 +0100

    Setup XCode project.

---
+++ ChatApp/ChatApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Mina Milosavljevic on 10/28/20.
+//  Copyright © 2020 Mina Milosavljevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
